5f611f96c8a5cff711360bed2dcea24531c5fecb
Marco Castignoli <marco@castignoli.it>
2022-07-01 20:20:16 +0200

    inital commit with gemcutter working

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,23 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
+require('hardhat-gemcutter');
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
-export default {
+const hardhatSettings = {
   solidity: "0.8.15",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_RINKEBY_URL,
+      },
+      timeout: 100000
+    },
+  },
 };
+
+export default hardhatSettings
