2e8ee33cd9442360423be2b8036aa572c124e3b0
Marco Castignoli <marco@castignoli.it>
2022-07-03 02:14:59 +0200

    format index and add clean examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,66 @@
 import type { NextPage } from 'next'
-import { useEtherBalance, useEthers } from '@usedapp/core'
-import { formatEther } from 'ethers/lib/utils'
+import { useEthers } from '@usedapp/core'
 import generateUseDiamond from '../utils/generateUseDiamond'
 import diamond from '../diamond.json'
 
 const Home: NextPage = () => {
   const { account, library, activateBrowserWallet } = useEthers()
 
-  const useDiamond = generateUseDiamond(diamond.address, account ? library?.getSigner() as any : undefined)
+  const useDiamond = generateUseDiamond(
+    diamond.address,
+    account ? (library?.getSigner() as any) : undefined
+  )
 
+  // For read functions
+  const balance = useDiamond.balanceOf(
+    '0xDEC3e07D46c46C089a323D62E60826D03716d7a2'
+  )
+
+  // For write functions
+  const { state: stateInit } = useDiamond.initMyToken
   const initToken = () => {
-    useDiamond.initMyToken.send('SD', 'sd', 8, '0xDEC3e07D46c46C089a323D62E60826D03716d7a2')
+    useDiamond.initMyToken.send(
+      'SD',
+      'sd',
+      8,
+      '0xDEC3e07D46c46C089a323D62E60826D03716d7a2'
+    )
   }
 
-  const balance = useDiamond.balanceOf('0xDEC3e07D46c46C089a323D62E60826D03716d7a2')
-
-
-  const etherBalance = useEtherBalance(account)
+  const { state: stateTransfer } = useDiamond.transfer
+  const transfer = () => {
+    useDiamond.transfer.send('0xF64D868cfDb1Ad4C3589452Ac541CB851e2E80e4', 10)
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => activateBrowserWallet()}>Connect</button>
       </div>
-      {account && <div>
-        <>
-        Account: {account}<br/>
-        Balance: {balance?.toString()}<br />
-        {etherBalance && (
-        <div className="balance">
-          Ether balance:
-          <p className="bold">{formatEther(etherBalance)} ETH</p>
+      {account && (
+        <div>
+          <>
+            Account: {account}
+            <br />
+            Balance: {balance?.toString()}
+            <br />
+            <button onClick={initToken}>init</button>
+            {(
+              {
+                PendingSignature: <>wait</>,
+                Exception: <>already initialized</>,
+              } as any
+            )[stateInit.status] || <></>}
+            <button onClick={transfer}>transfer</button>
+            {(
+              {
+                PendingSignature: <>wait</>,
+                Exception: <>error</>,
+              } as any
+            )[stateTransfer.status] || <></>}
+          </>
         </div>
       )}
-        <button onClick={initToken}>init</button>
-        </>
-      </div>}
     </div>
   )
 }
